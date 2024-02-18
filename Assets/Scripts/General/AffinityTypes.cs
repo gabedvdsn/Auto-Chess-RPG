6505525dd0a32916e488d64d7893fe7d6f9214ca
gabedvdsn <73317121+gabedvdsn@users.noreply.github.com>
2024-02-17 20:30:10 -0800

    Initial Commit

---
+++ Assets/Scripts/General/AffinityTypes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace AutoChessRPG.Affinity
+{
+    public enum Affinity
+    {
+        Fire,
+        Water,
+        Earth,
+        Air,
+        Void,
+        Plague
+    }
+}
